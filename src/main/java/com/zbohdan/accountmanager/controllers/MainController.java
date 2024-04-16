219fbaddbf1d68cbe0b009b51c642a6e01dff45b
Bogdan <zbohdan7@gmail.com>
2024-04-16 17:48:23 +0300

    Added welcome page.

---
+++ src/main/java/com/zbohdan/accountmanager/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.zbohdan.accountmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public ModelAndView welcome() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("welcome");
+        mv.getModel().put("welcome", "Hello, World!");
+        return mv;
+    }
+
+}
